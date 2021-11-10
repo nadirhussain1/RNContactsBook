05ec13f939703f064b4875ee476d290609584ea6
Nadir Hussain <nadirhussain1@gmail.com>
2021-11-10 19:04:31 +0500

    - changed color of navigation bar on profile screen.

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Contact from './screens/contacts';
 import Profile from './screens/profile';
+import colors from './utils/colors';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -19,7 +21,14 @@ export default function App(){
         <Stack.Screen
           name='Profile'
           component={Profile}
-          options={{title:'Profile'}}
+          options={
+            {
+              title:'Profile',
+              headerStyle:{backgroundColor:colors.blue},
+              headerTintColor:'white',
+            }
+
+          }
         />
 
       </Stack.Navigator>
