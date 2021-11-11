3b1f1567356816ec45b60a304e5b1f5357f3346c
Nadir Hussain <nadirhussain1@gmail.com>
2021-11-11 11:26:44 +0500

    - tab navigation added. - stack navigation nested in tab navigation.

--- App.js
+++ App.js
@@ -1,47 +1,107 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Contact from './screens/contacts';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import Contacts from './screens/contacts';
 import Profile from './screens/profile';
 import Favourites from './screens/favourites';
+import User from './screens/user';
 
 import colors from './utils/colors';
 
 
-const Stack = createNativeStackNavigator();
+const ContactsStack = createNativeStackNavigator();
+const FavouritesStack = createNativeStackNavigator();
+const UserStack = createNativeStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+
+function ContactsStackScreens(){
+  return(
+    <ContactsStack.Navigator
+      initialRouteName='List'  >
+
+      <ContactsStack.Screen
+        name='List'
+        component={Contacts}
+        options={{headerShown: false}}
+
+      />
+
+      <ContactsStack.Screen
+        name='Profile'
+        component={Profile}
+        options={{headerShown: false}}
+      />
+
+    </ContactsStack.Navigator>
+  );
+}
+
+function FavouritesStackScreens(){
+  return(
+    <FavouritesStack.Navigator
+      initialRouteName='Favourites'  >
+
+      <FavouritesStack.Screen
+        name='Favourites'
+        component={Favourites}
+        options={{headerShown: false}}
+      />
+
+      <FavouritesStack.Screen
+        name='Profile'
+        component={Profile}
+        options={{headerShown: false}}
+
+      />
+
+    </FavouritesStack.Navigator>
+  );
+}
+
+function UserStackScreens(){
+  return(
+    <UserStack.Navigator
+      initialRouteName='User'  >
+
+      <UserStack.Screen
+        name='User'
+        component={User}
+        options={{headerShown: false}}
+      />
+
+
+    </UserStack.Navigator>
+  );
+}
 
 export default function App(){
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Favourite'
-      >
-        <Stack.Screen
-          name='List'
-          component={Contact}
-          options={{title:'Contacts'}}
-        />
+    <Tab.Navigator
+      screenOptions={{
+         headerShown: false
+      }}>
+           <Tab.Screen
+             name="Contacts"
+             component={ContactsStackScreens}
+           />
+           <Tab.Screen
+             name="Favourites"
+             component={FavouritesStackScreens}
 
-        <Stack.Screen
-          name='Favourite'
-          component={Favourites}
-          options={{title:'Favourites'}}
-        />
+           />
+           <Tab.Screen
+             name="Me"
+             component={UserStackScreens}
 
-        <Stack.Screen
-          name='Profile'
-          component={Profile}
-          options={
-            {
-              title:'Profile',
-              headerStyle:{backgroundColor:colors.blue},
-              headerTintColor:'white',
-            }
+           />
 
-          }
-        />
-
-      </Stack.Navigator>
+         </Tab.Navigator>
     </NavigationContainer>
   );
 }
